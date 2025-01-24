ed6d126c51e00eafb352f416a9a8673181047096
Okenamay <yamabaka@yandex.ru>
2025-01-24 07:27:21 +0300

    4.2. Minor rules fix

--- daterules/rules.go
+++ daterules/rules.go
@@ -10,7 +10,7 @@ import (
 const TimeFormat string = "20060102"
 
 type Task struct {
-	Id      string `json:"id"`
+	ID      string `json:"id"`
 	Date    string `json:"date"`
 	Title   string `json:"title"`
 	Comment string `json:"comment"`
